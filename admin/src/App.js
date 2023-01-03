049d89834cb008a7cb6d883e844f20bb8834b728
UMAR-151 <SP20-BCS-151@CUILAHORE.EDU.PK>
2023-01-03 15:03:43 +0500

    edited

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,44 +1,28 @@
-import React from "react";
-import Login from "./components/views/auth/Login";
-import Register from "./components/views/auth/Register";
-import Products from "./products/Products";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
-import ProductDetails from "./products/ProductDetails";
-import ProductForm from "./products/ProductForm";
-import { Container } from "@mui/material";
-import Provider from "react-redux/es/components/Provider";
-import store from "./store";
-import NavBar from "./components/layout/NavBar";
-import MenuColorChanger from "./components/redux-examples/MenuColorChanger";
-import ReduxThunkExample from "./components/redux-examples/ReduxThunkExample";
+import React from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+import Home from './components/layout/page/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Cards from './components/layout/Cards';
+import BodySection from './components/layout/BodySection';
+
+
 function App() {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <Router basename={"/admin"}>
-          <NavBar />
-          <Container maxWidth="lg">
-            <Routes>
-              <Route
-                path="/redux-thunk-example"
-                element={<ReduxThunkExample />}
-              />
-              <Route path="/redux-example" element={<MenuColorChanger />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/products/create" element={<ProductForm />} />
-              <Route path="/products/edit/:id" element={<ProductForm />} />
-              <Route
-                path="/products/details/:id"
-                element={<ProductDetails />}
-              />
-              <Route path="/" element={<Products />} />
-            </Routes>
-          </Container>
-        </Router>
-      </Provider>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <BodySection />
+        <Cards/>
+        <Footer/>
+        <Routes>
+
+          <Route path='/' exact component={Home} />
+        
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ admin/src/components/layout/page/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../../App';
+import Cards from '../Cards';
+import Footer from '../Footer';
+import BodySection from '../BodySection';
+
+
+function Home() {
+  return (
+    <>
+      <BodySection />
+      <Cards />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
